cac6c832b5df51d0509bbf5389be41ff7dc6ea4f
Joe Carre <joe.carre@rfa.co.uk>
2018-04-20 16:51:00 +0100

    Search bar
    -added a search bar to header
    -currently only prints info from api to the console

--- react-coin/src/components/common/Header.js
+++ react-coin/src/components/common/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import logo from './logo.png';
+import Search from './Search';
 
 const containerStyle = {
     fontSize: '40px'
@@ -13,6 +14,8 @@ const Header = () => {
             <Link to="/">
                 <img src={logo} alt="logo" className="Header-logo"/>
             </Link>
+
+            <Search />
         </div>
     );
 }
