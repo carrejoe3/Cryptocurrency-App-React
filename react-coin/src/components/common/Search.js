88469c69153b514a02350dd28b2ed1ce579b0707
Joe <carre.joe3@gmail.com>
2018-04-22 18:22:26 +0100

    Search bar finishing touches
    -Search bar now directs user to currency detail panel on click

--- react-coin/src/components/common/Search.js
+++ react-coin/src/components/common/Search.js
@@ -1,61 +1,114 @@
-import React from 'react';
-import Loading from '../common/Loading';
-import { API_URL } from '../../config';
-import { handleResponse } from '../../helpers';
-import './Search.css';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import Loading from "./Loading";
+import { API_URL } from "../../config";
+import { handleResponse } from "../../helpers";
+import "./Search.css";
 
 class Search extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            searchQuery: '',
-            loading: false,
-        };
+    this.state = {
+      searchResults: [],
+      searchQuery: "",
+      loading: false
+    };
 
-        this.handleChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleRedirect = this.handleRedirect.bind(this);
+  }
+
+  handleChange(event) {
+    const searchQuery = event.target.value;
+
+    this.setState({ searchQuery });
+
+    // If searchQuery isn't present, don't send request to server
+    if (!searchQuery) {
+      return "";
     }
 
-    handleChange(event) {
-        const searchQuery = event.target.value;
+    this.setState({ loading: true });
 
-        this.setState({ searchQuery });
+    fetch(`${API_URL}/autocomplete?searchQuery=${searchQuery}`)
+      .then(handleResponse)
+      .then(result => {
+        this.setState({
+          loading: false,
+          searchResults: result
+        });
+      });
+  }
 
-        // If user hasnt entered search query, dont send request
-        if(!searchQuery) {
-            return '';
-        }
+  handleRedirect(currencyId) {
+    // Clear input value and close autocomplete container,
+    // By clearing searchQuery state
+    this.setState({
+      searchQuery: "",
+      searchResults: []
+    });
 
-        this.setState({ loading: true });
+    this.props.history.push(`/currency/${currencyId}`);
+  }
 
-        fetch(`${API_URL}/autocomplete?searchQuery=${searchQuery}`)
-            .then(handleResponse)
-            .then((result) => {
-                console.log(result);
+  renderSearchResults() {
+    const { searchResults, searchQuery, loading } = this.state;
 
-                this.setState({ loading: false });
-            });
+    if (!searchQuery) {
+      return "";
     }
 
-    render() {
-        const { loading } = this.state;
-
-        return (
-            <div className="Search">
-                <span className="Search-icon" />
-
-                <input className="Search-input" type="text" placeholder="Currency name" onChange={this.handleChange}/>
-
-                {loading  &&
-                <div className="Search-loading">
-                    <Loading
-                        width='12px'
-                        height='12px'
-                    />
-                </div>}
+    if (searchResults.length > 0) {
+      return (
+        <div className="Search-result-container">
+          {searchResults.map(result => (
+            <div
+              key={result.id}
+              className="Search-result"
+              onClick={() => this.handleRedirect(result.id)}
+            >
+              {result.name} ({result.symbol})
             </div>
-        );
+          ))}
+        </div>
+      );
     }
+
+    if (!loading) {
+      return (
+        <div className="Search-result-container">
+          <div className="Search-no-result">No results found.</div>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    const { loading, searchQuery } = this.state;
+
+    return (
+      <div className="Search">
+        <span className="Search-icon" />
+
+        <input
+          className="Search-input"
+          type="text"
+          placeholder="Currency name"
+          onChange={this.handleChange}
+          value={searchQuery}
+        />
+
+        {loading && (
+          <div className="Search-loading">
+            <Loading width="12px" height="12px" />
+          </div>
+        )}
+
+        {this.renderSearchResults()}
+      </div>
+    );
+  }
 }
 
-export default Search;
+export default withRouter(Search);
