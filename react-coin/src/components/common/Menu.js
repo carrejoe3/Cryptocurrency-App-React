6e510b6753049681f734593e98e29aa1b1fb4d78
Joe Carre <joe.carre@rfa.co.uk>
2018-04-23 13:52:43 +0100

    Burger Menu
    -added a burger menu to the top right hand side of the header
    -added animation css which toggles onclick

--- react-coin/src/components/common/Menu.js
+++ react-coin/src/components/common/Menu.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './Menu.css';
 
-const Menu = () => {
+class Menu extends React.Component {
 
-    return (
-        <div>Menu</div>
-    );
+    constructor() {
+        super();
+
+        this.state = {
+            showHideSidenav: "closed",
+        };
+
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    toggleMenu() {
+        let displayState = this.state.showHideSidenav == "closed"? "open": "closed";
+        this.setState({ showHideSidenav: displayState});
+    };
+
+    render() {
+        return (
+            <div id="nav-icon1" onClick={() => this.toggleMenu()} className={this.state.showHideSidenav}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        );
+    }
 }
 
 export default Menu;
